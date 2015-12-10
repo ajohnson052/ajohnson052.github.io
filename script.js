f5e97eb31b0ae674fcac5528540843509d57679a
Alexis Johnson <alexis.johnson052@gmail.com>
2015-12-09 23:31:37 -0500

    move contact icons to last section

--- script.js
+++ script.js
@@ -1,13 +1,12 @@
 $(document).ready(function(){
   $(window).scroll(function(){
     console.log($(window).scrollTop())
-    if ($(window).scrollTop() < $(".home").height()){
+    if ($(window).scrollTop() <= $("#home").height()){
       console.log('top')
       $("nav").css("position", "relative")
     }
     else{
       $("nav").css("position", "fixed")
-      $("nav").css("top", "5vh")
     }
   })
 })
