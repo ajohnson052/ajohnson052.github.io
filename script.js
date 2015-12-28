853a1432a312bbc744fe33bf9520a649e402cf21
Alexis Johnson <alexis.johnson052@gmail.com>
2015-12-28 12:27:40 -0600

    tree image for background, color for descriptors

--- script.js
+++ script.js
@@ -1,6 +1,5 @@
 $(document).ready(function(){
   $(window).scroll(function(){
-    console.log($(window).scrollTop())
     if ($(window).scrollTop() <= $("#home").height()){
       $("nav").css("position", "relative")
     }
@@ -8,4 +7,27 @@ $(document).ready(function(){
       $("nav").css("position", "fixed")
     }
   })
+  var descriptors = ["Web Developer", "Nature Enthusiast", "Economist", "Rock Climber", "Thinker", "Beer Lover", "Singer", "Biker", "Marathon Runner", "Tree Hugger", "Dancer", "Health Nut", "Yogini", "Traveler", "Rubyist", "Musician"]
+  d3.select("#section1")
+    .select(".content")
+    .selectAll("h2")
+    .data(descriptors)
+    .enter()
+    .append("h2")
+    .text(function(d){
+      if(d === descriptors[descriptors.length - 1]){
+        return d;
+      }else{
+        return d + " | ";
+      }
+    })
+    .style("display", "inline")
+    .style("color", function(){
+      var r = Math.floor(Math.random()*250)
+      var g = Math.floor(Math.random()*250)
+      var b = Math.floor(Math.random()*250)
+
+      return "rgb("+ r + "," + g + "," + b + ")";
+    })
+
 })
