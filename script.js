bf595744e5304b48d38ee03060785f354462fc9b
Alexis Johnson <alexis.johnson052@gmail.com>
2016-01-03 10:09:25 -0500

    improve mobile design

--- script.js
+++ script.js
@@ -1,10 +1,12 @@
 $(document).ready(function(){
   $(window).scroll(function(){
-    if ($(window).scrollTop() <= $("#home").height()){
-      $("nav").css("position", "relative")
-    }
-    else{
-      $("nav").css("position", "fixed")
+    if($(window).width() > 400){
+      if ($(window).scrollTop() <= $("#home").height()){
+        $("nav").css("position", "relative")
+      }
+      else{
+        $("nav").css("position", "fixed")
+      }
     }
   })
   var descriptors = ["Web Developer", "Nature Enthusiast", "Economist", "Rock Climber", "Thinker", "Beer Lover", "Singer", "Biker", "Marathon Runner", "Tree Hugger", "Dancer", "Health Nut", "Yogini", "Traveler", "Rubyist", "Musician"]
