05c522fc76ced5a7bdf9d80dc8b2b40041c83d5e
Alexis Johnson <alexis.johnson052@gmail.com>
2016-02-04 22:16:43 -0500

    add sticky header on scroll for mobile

--- script.js
+++ script.js
@@ -1,17 +1,15 @@
 $(document).ready(function(){
   $(window).scroll(function(){
-    if($(window).width() > 400){
-      if ($(window).scrollTop() <= $("#home").height()){
-        $("nav").css("position", "relative");
-        $("nav").css("background", "none");
-        $(".spacer").css("display", "none");
-      }
-      else{
-        $("nav").css("position", "fixed");
-        $("nav").css("width", "100vw")
-        $("nav").css("background", "inherit");
-        $(".spacer").css("display", "block");
-      }
+    if ($(window).scrollTop() <= $("#home").height()){
+      $("nav").css("position", "relative");
+      $("nav").css("background", "none");
+      $(".spacer").css("display", "none");
+    }
+    else{
+      $("nav").css("position", "fixed");
+      $("header").css("background", "inherit");
+      $("nav").css("background", "inherit");
+      $(".spacer").css("display", "block");
     }
   });
   var descriptors = ["Web Developer", "Nature Junkie", "Economist", "Rock Climber", "Thinker", "Beer Enthusiast", "Singer", "Biker", "Marathon Runner", "Tree Hugger", "Dancer", "Health Nut", "Yogini", "Traveler", "Rubyist", "Musician"];
